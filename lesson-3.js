313e6f6d780912262ea4274880b4969349185752
Sergei <1og@mail.ru>
2021-08-21 20:14:40 +0600

    upd lesson-3

--- lesson-3.js
+++ lesson-3.js
@@ -42,7 +42,6 @@
 {
     let massage = 'x ';
     for (let i = 0; i < 20; i++) {
-        console.log('\n');
         let str = '';
         for (let j = 0; j <= i; j++) {
             str = str + massage;
