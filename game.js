93d09d0d9daa18e567242c6d20e7350846d8a71f
Sergei <1og@mail.ru>
2021-08-28 23:05:56 +0600

    upd chess

--- game.js
+++ game.js
@@ -44,21 +44,21 @@
     function getAlphabet(a) {
         switch (a) {
             case 1:
-                return 'A';
-            case 2:
-                return 'B';
-            case 3:
-                return 'C';
-            case 4:
-                return 'D';
-            case 5:
-                return 'E';
-            case 6:
-                return 'F';
-            case 7:
-                return 'G';
-            case 8:
                 return 'H';
+            case 2:
+                return 'G';
+            case 3:
+                return 'F';
+            case 4:
+                return 'E';
+            case 5:
+                return 'D';
+            case 6:
+                return 'C';
+            case 7:
+                return 'B';
+            case 8:
+                return 'A';
         }
     }
 
