0909440fc358db4cf93d5831f3ed1a525ad95cc9
Sergei <1og@mail.ru>
2021-08-29 22:46:45 +0600

    upd new basket

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 'use strict';
-
-const blockBasket = document.getElementById('basket');
-
-function checkBasket() {
-    if (basket.productList.length > 0)
-        return true;
-    else
-        return false;
-}
-
-function showBasketMaintenance() {
-    if (checkBasket() == true) {
-        blockBasket.innerText = `В коризе: ${basket.countBasketQuantity()} товара на сумму ${basket.countBasketPrice()} рублей.`;
-    } else
-        blockBasket.innerText = 'Корзина пуста';
+const cartItem = {
+    render(good) {
+        return `<div class="good">
+                    <div><b>Наименование</b>: ${good.name}</div>
+                    <div><b>Цена за шт.</b>: ${good.price}</div>
+                    <div><b>Количество</b>: ${good.quantity}</div>
+                    <div><b>Стоимость</b>: ${good.quantity * good.price}</div>
+                </div>`;
+    }
 }
 
 const basket = {
-    productList: [],
+    blockBasket: null,
+    cartItem,
+    productList: [{
+        name: 'Бананы',
+        quantity: 5,
+        price: 200,
+    }, {
+        name: 'Груши',
+        quantity: 4,
+        price: 150,
+    }, {
+        name: 'Апельсины',
+        quantity: 7,
+        price: 215,
+    }, ],
+
+    init() {
+        this.blockBasket = document.querySelector('.basket');
+        const basketBtn = document.createElement('button');
+        this.blockBasket.appendChild(basketBtn);
+        basketBtn.classList.add('basket-btn')
+        basketBtn.addEventListener('click', this.clear.bind(this));
+        basketBtn.innerHTML = 'Очистить корзину';
+
+        this.render();
+    },
+    render() {
+        if (this.productList.length) {
+            this.productList.forEach(good => {
+                this.blockBasket.insertAdjacentHTML('afterbegin', this.cartItem.render(good));
+            });
+            this.blockBasket.insertAdjacentHTML('beforeend', `В коризе: ${this.countBasketQuantity()} товара на сумму ${this.countBasketPrice()} рублей.`);
+        } else {
+            this.blockBasket.textContent = 'Корзина пуста';
+        }
+    },
     countBasketQuantity() {
         return this.productList.reduce((totalQuantity, item) => totalQuantity += item.quantity, 0);
     },
@@ -26,64 +55,70 @@ const basket = {
             return totalPrice + item.quantity * item.price;
         }, 0);
     },
-}
+    clear() {
+        this.productList = [];
+        this.render();
+    },
+};
+
+basket.init();
 
 
 
-//3-ее задание
-const product = [{
-    name: 'banans',
-    quantity: 1,
-    price: 200,
-}, {
-    name: 'pears',
-    quantity: 1,
-    price: 150,
-}, {
-    name: 'orange',
-    quantity: 1,
-    price: 215,
-}]
+// //3-ее задание
+// const product = [{
+//     name: 'Бананы',
+//     quantity: 1,
+//     price: 200,
+// }, {
+//     name: 'Грушы',
+//     quantity: 1,
+//     price: 150,
+// }, {
+//     name: 'Апельсины',
+//     quantity: 1,
+//     price: 215,
+// }]
 
-const blockProduct = document.getElementById('catalog');
-blockProduct.style.width = '150px';
-blockProduct.style.height = '150px';
-blockProduct.style.marginTop = '50px';
-blockProduct.style.border = '1px solid black'
+// const blockProduct = document.getElementById('catalog');
+// blockProduct.style.width = '150px';
+// blockProduct.style.height = '150px';
+// blockProduct.style.marginTop = '50px';
+// blockProduct.style.border = '1px solid black'
 
-const banansAdd = document.createElement('button');
-blockProduct.appendChild(banansAdd);
-banansAdd.innerHTML = 'Add banana to basket';
+// const banansAdd = document.createElement('button');
+// blockProduct.appendChild(banansAdd);
+// banansAdd.innerHTML = 'Add banana to basket';
 
-function addBananaToProductList() {
-    basket.productList.push(product[0]);
-    showBasketMaintenance();
-}
-// banansAdd.onclick = addBananaToProductList;
-banansAdd.addEventListener('click', addBananaToProductList);
+// function addBananaToProductList() {
+//     basket.productList.push(product[0]);
+//     showBasketMaintenance();
+// }
+// // banansAdd.onclick = addBananaToProductList;
+// banansAdd.addEventListener('click', addBananaToProductList);
 
-const orangeAdd = document.createElement('button');
-orangeAdd.style.marginTop = '30px';
-blockProduct.appendChild(orangeAdd);
-orangeAdd.innerHTML = 'Add orange to basket';
+// const orangeAdd = document.createElement('button');
+// orangeAdd.style.marginTop = '30px';
+// blockProduct.appendChild(orangeAdd);
+// orangeAdd.innerHTML = 'Add orange to basket';
 
-function addOrangeToProductList() {
-    basket.productList.push(product[2]);
-    showBasketMaintenance();
-}
+// function addOrangeToProductList() {
+//     basket.productList.push(product[2]);
+//     showBasketMaintenance();
+// }
 
-orangeAdd.addEventListener('click', addOrangeToProductList);
+// orangeAdd.addEventListener('click', addOrangeToProductList);
 
-const pearsAdd = document.createElement('button');
-pearsAdd.style.marginTop = '30px';
-blockProduct.appendChild(pearsAdd);
-pearsAdd.innerHTML = 'Add pears to basket';
+// const pearsAdd = document.createElement('button');
+// pearsAdd.style.marginTop = '30px';
+// blockProduct.appendChild(pearsAdd);
+// pearsAdd.innerHTML = 'Add pears to basket';
 
-function addPearsToProductList() {
-    basket.productList.push(product[1]);
-    showBasketMaintenance();
-}
+// function addPearsToProductList() {
+//     basket.productList.push(product[1]);
+//     showBasketMaintenance();
+// }
 
-pearsAdd.addEventListener('click', addPearsToProductList);
+// pearsAdd.addEventListener('click', addPearsToProductList);
 
-showBasketMaintenance();
+// showBasketMaintenance();
