3ff1c44aca94677bf12c874a39854ff0e16d3894
Sergei <1og@mail.ru>
2021-08-31 21:51:05 +0600

    upd catalog

--- script.js
+++ script.js
@@ -3,10 +3,13 @@
 const cartItem = {
     render(good) {
         return `<div class="good">
-                    <div><b>Наименование</b>: ${good.name}</div>
-                    <div><b>Цена за шт.</b>: ${good.price}</div>
-                    <div><b>Количество</b>: ${good.quantity}</div>
-                    <div><b>Стоимость</b>: ${good.quantity * good.price}</div>
+                    <div><img src="${good.image}"></div>
+                    <div>
+                        <div><b>Наименование</b>: ${good.name}</div>
+                        <div><b>Цена за шт.</b>: ${good.price}</div>
+                        <div><b>Количество</b>: ${good.quantity}</div>
+                        <div><b>Стоимость</b>: ${good.quantity * good.price}</div>
+                    </div>
                 </div>`;
     }
 }
@@ -14,19 +17,7 @@ const cartItem = {
 const basket = {
     blockBasket: null,
     cartItem,
-    productList: [{
-        name: 'Бананы',
-        quantity: 5,
-        price: 200,
-    }, {
-        name: 'Груши',
-        quantity: 4,
-        price: 150,
-    }, {
-        name: 'Апельсины',
-        quantity: 7,
-        price: 215,
-    }, ],
+    productList: [],
 
     init() {
         this.blockBasket = document.querySelector('.basket');
@@ -45,7 +36,7 @@ const basket = {
             });
             this.blockBasket.insertAdjacentHTML('beforeend', `В коризе: ${this.countBasketQuantity()} товара на сумму ${this.countBasketPrice()} рублей.`);
         } else {
-            this.blockBasket.textContent = 'Корзина пуста';
+            this.blockBasket.insertAdjacentHTML('afterbegin', 'Корзина пуста');
         }
     },
     countBasketQuantity() {
@@ -63,3 +54,71 @@ const basket = {
 };
 
 basket.init();
+
+const catalogItem = {
+    render(good) {
+        return `<div class="good">
+                    <div><img src="${good.image}"></div>
+                    <div>
+                        <div><b>Наименование</b>: ${good.name}</div>
+                        <div><b>Цена за шт.</b>: ${good.price}</div>
+                        <div><b>Количество</b>: ${good.quantity}</div>
+                        <div><b>Стоимость</b>: ${good.quantity * good.price}</div>
+                    </div>
+                    <div><button class="catalog-btn" data-id = '${good.id}'>Купить</button></div>
+                </div>`;
+    }
+}
+
+const catalog = {
+    catalogBlock: null,
+    catalogItem,
+    productList: [{
+        id:1,
+        image: './img/banana.png',
+        name: 'Бананы',
+        quantity: 5,
+        price: 200,
+    }, {
+        id:2,
+        image: './img/pear.png',
+        name: 'Груши',
+        quantity: 4,
+        price: 150,
+    }, {
+        id:3,
+        image: './img/orange.png',
+        name: 'Апельсины',
+        quantity: 7,
+        price: 215,
+    },],
+
+    init() {
+        this.catalogBlock = document.querySelector('.catalog');
+        this.catalogBlock.addEventListener('click', (event) => {
+            if(event.target.tagName !== 'BUTTON') return;
+            this.addObjToCart(event.target.dataset.id);
+        });
+
+        this.render();
+    },
+
+    render() {
+        this.productList.forEach(good => {
+                this.catalogBlock.insertAdjacentHTML('afterbegin', this.catalogItem.render(good));
+            });
+    },
+
+    findProduct(key) {
+        for(let i = 0; i < this.productList.length; i ++){
+            let arr = Object.values(this.productList[i]);
+            if(arr.includes(Number(key))) return this.productList[i];
+            else continue;
+        }
+    },
+    addObjToCart(key) {
+        basket.productList.push(this.findProduct(key));
+        basket.render();
+    },
+}
+catalog.init();
